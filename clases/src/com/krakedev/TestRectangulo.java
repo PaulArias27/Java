0883fee236a26b994aeb3c579b60ea2143100506
PaulArias27 <paularias2727@gmail.com>
2025-12-11 23:19:56 -0500

    se relaizo la funcion calcularPerimetro del reto 10

---
+++ clases/src/com/krakedev/TestRectangulo.java
@@ -0,0 +1,34 @@
+package com.krakedev;
+
+public class TestRectangulo {
+
+	public static void main(String[] args) {
+		Rectangulo r1;
+		r1 = new Rectangulo();
+		Rectangulo r2;
+		r2 = new Rectangulo();
+		
+		int areaR1;
+		int areaR2;
+		double perimetro1;
+		double perimetro2;
+		
+		r1.base = 4;
+		r1.altura = 2;
+		
+		r2.base = 8;
+		r2.altura = 3;
+		
+		areaR1 = r1.calcularArea();
+		areaR2 = r2.calcularArea();	
+		perimetro1 = r1.calcularPerimetro();
+		perimetro2 = r2.calcularPerimetro();
+		
+		System.out.println("area de r1: "+ areaR1);
+		System.out.println("area de r2: "+ areaR2);
+		System.out.println("el preimetro de r1: "+ perimetro1);
+		System.out.println("el preimetro de r2: "+ perimetro2);
+		
+	}
+
+}
